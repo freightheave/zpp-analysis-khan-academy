6e26dc4438a4d19f8b89d829b41e0f4e4860c180
Roshan Komaravolu <17759116+freightheave@users.noreply.github.com>
2024-07-02 03:55:04 +0530

    Final Commit
    Add final PPT, final code changes, final graphs.

--- KA_Script.R
+++ KA_Script.R
@@ -138,11 +138,13 @@ ggplot(data = long_reg_stu, aes(x = Month, y = Active.Students, group = School.N
 
 # ---------- Per school plot ---------
 
-school_name_plot <- reg_stu[reg_stu$School.Name == "ZPP 69",]
+school_name <- "ZPP 69"
+
+school_name_plot <- reg_stu[reg_stu$School.Name == school_name,]
 #storing name of school for plotting, change 'school in RHS to vary the school.
 #Now, need to convert school_name_plot into a long df.
 
-specific_school_mean <- mean_active_students_school %>% filter(School.Name == "ZPP 69")
+specific_school_mean <- mean_active_students_school %>% filter(School.Name == school_name)
 #specific mean picked from mean_active_students_school to overlay on to total graph.
 
 long_school_name_plot <- school_name_plot %>% 
@@ -162,9 +164,6 @@ ggplot() +
   geom_line(data = long_active_students_avg, aes(x = Month, y = Active.Students, group = Average.Active.Students, color = Average.Active.Students), linetype = "dotdash") +
   geom_point(data = long_active_students_avg, aes(x = Month, y = Active.Students, group = Average.Active.Students, color = Average.Active.Students)) +
   geom_label(data = long_active_students_avg, aes(x = Month, y = Active.Students, label = paste(round(Active.Students, 2)))) +
-  scale_colour_manual(name = "Legend", values = c("#CC0000", "#006600", "#669999", "#00CCCC", 
-                                                  "#660099", "#CC0066", "#FF9999", "#FF9900", 
-                                                  "black", "black", "black", "black", "black")) +
   geom_hline(yintercept = specific_school_mean$mean_active_students_school, colour = "#FF9900", linetype = "dashed") +
   geom_label(aes(
     x = max(as.numeric(long_school_name_plot$Month)),
@@ -176,8 +175,9 @@ ggplot() +
   
  # ------------ Per district Plots ------------
  
+district_number <- 3
  
- long_district_level_ASPM <- district_level_ASPM[[3]] %>%
+long_district_level_ASPM <- district_level_ASPM[[district_number]] %>%
    pivot_longer(cols = starts_with("ASPM.M"), 
                 names_to = "Month", 
                 values_to = "Active.Students")
@@ -185,10 +185,10 @@ ggplot() +
 
 long_district_level_ASPM$Month <- factor(long_district_level_ASPM$Month, levels = paste0("ASPM.M", 1:12), ordered = TRUE)
 
-plot <- ggplot(data = long_district_level_ASPM, aes(x = Month, y = Active.Students, group = School.Name, colour = School.Name)) +
+ggplot(data = long_district_level_ASPM, aes(x = Month, y = Active.Students, group = School.Name, colour = School.Name)) +
   geom_line() +
   geom_point() +
-  labs(x = "Month", y = "Active Students", title = str_glue("Monthly Data for {district_level_ASPM[[3]]$District.Name}"))
+  labs(x = "Month", y = "Active Students", title = paste0("Monthly Data for District ", district_number))
   #geom_label(aes(label = Active.Students))
   #omitting for legibility
 
@@ -228,7 +228,9 @@ long_medians <- median_percentages_reg_stu %>%
 
 # ----------- Per School percentage calc. -----------
 
-percent_school_plot <- reg_stu_with_percent[reg_stu_with_percent$School.Name == "ZPP 145",]
+percent_school_name <- "ZPP 145"
+
+percent_school_plot <- reg_stu_with_percent[reg_stu_with_percent$School.Name == percent_school_name,]
 # storing name of school for plotting, change 'school in RHS to vary the school.
 # Now, need to convert school_name_plot into a long df.
 
@@ -263,18 +265,111 @@ ggplot() +
     label = "Max"
   )) +
   scale_y_continuous(breaks = seq(0, 150, by = 10)) +
-  geom_line(data=long_medians, aes(x = Month, y = Median.Percentages, group = 1)) +
-  geom_point(data=long_medians, aes(x = Month, y = Median.Percentages, group = 1)) +
-  geom_label(data = long_medians, aes(x = Month, y = Median.Percentages, label = paste0(round(Median.Percentages,1))))
+  geom_line(data=long_medians, aes(x = Month, y = Median.Percentages, colour = "Median", group = 1)) +
+  geom_point(data=long_medians, aes(x = Month, y = Median.Percentages, colour = "Median", group = 1)) +
+  geom_label(data = long_medians, aes(x = Month, y = Median.Percentages, label = paste0(round(Median.Percentages,1)))) +
+  labs(x = "Month", y = "Active Students as a % of Total Registrations", title = paste0("Percentage Data for ", percent_school_plot$School.Name))
+   
+
+## ------------ Hours worked metric ------------
+
+#Need to plot ideal hours - 30 * registered students [Overlay]
+#Calculated - 30 mins * Active Students
+#Real - real life.
+
+string = "ZPP 145"
+
+df_hours <- act_hours[act_hours$School.Name == string,]
+
+long_df_hours <- df_hours %>% 
+  pivot_longer(
+    cols = starts_with("ALOKPAS.M"),
+    names_to = ("Month"),
+    values_to = ("Avg.Learning.Time")
+  )
+#long data format for plotting learning hours.
+
+long_df_hours$Month <- factor(long_df_hours$Month, levels = paste0("ALOKPAS.M", 1:12), ordered = TRUE)
+
+ggplot() +
+  geom_line(data = long_df_hours, aes(x = Month, y = Avg.Learning.Time, colour = School.Name, group = School.Name)) +
+  geom_point(data = long_df_hours, aes(x = Month, y = Avg.Learning.Time, colour = School.Name, group = School.Name)) +
+  geom_label(data = long_df_hours, aes(x = Month, y = Avg.Learning.Time, label = Avg.Learning.Time)) +
+  geom_hline(yintercept = 30, linetype = "dashed") +
+  geom_label(aes(
+    x = length(long_df_hours$Month) - 2,
+    y = 30,
+    label = "Ideal Learning Time (mins)"
+  )) +
+  labs(x = "Month", y = "Avg. Learning Per Student (mins)", title = paste0("Learning Time data for ", string))
+
+
+## ------ District Level ---------
+
+district_number_LH <- 3
+
+long_district_level_ALOKPAS <- district_level_ALOKPAS[[district_number_LH]] %>%
+  pivot_longer(cols = starts_with("ALOKPAS.M"), 
+               names_to = "Month", 
+               values_to = "Avg.Learning")
+# Change the value inside [[]] to the corresponding district number (1 to 15)
+
+long_district_level_ALOKPAS$Month <- factor(long_district_level_ALOKPAS$Month, levels = paste0("ALOKPAS.M", 1:12), ordered = TRUE)
+
+ggplot(data = long_district_level_ALOKPAS, aes(x = Month, y = Avg.Learning, group = School.Name, colour = School.Name)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Month", y = "Average Learning per Student  (mins) ", title = paste0("Monthly Data for District ", district_number_LH))
+#geom_label(aes(label = Avg.Learning))
+
+
+## Find list of schools who performed the best!
+## Criteria is to have avg learning on KA more than the recommended amount of 30 mins.
+## Answer is ZPP 42, 45, 70, 71, 91, 103, 124.
+
+performers_hours <- list()
+
+for (school_number in 1:155) {
+  hours_val = 0
+  for (cols in 3:14) {
+    if(act_hours[school_number, cols] >= 30) {
+      hours_val = hours_val + 1
+    }
+  }
+  
+  if (hours_val >= 9) {
+    len <- length(performers)
+    performers[[len+1]] <- (act_hours[school_number,2])
+  }
+}
+
+## Find list of schools who consistently had more than the avg number of active users for that month.
+
+active_performers <- list()
+
+for (school_number in 1:155) {
+  students_val = 0
+  for (cols in 4:15) {
+    if(reg_stu[school_number, cols] >= active_students_avg[1,(cols-3)]) {
+      students_val = students_val + 1
+    }
+  }
+  
+  if (students_val >= 9) {
+    len <- length(performers)
+    active_performers[[len+1]] <- (reg_stu[school_number,2])
+  }
+}
 
 
 
 
- 
 
-# TODO:
-# Work on hours on KA
-# Compare schools as a percentage of participation - highest in district.
+
+
+
+
+
 
 
 
--- Plot generators.R
+++ Plot generators.R
@@ -1,3 +1,6 @@
+#This part of the code is separate from the original R file as this code generates PNGs of the plots to a specified folder.
+
+
 # ---------------- DISTRICT LEVEL PLOT GENERATOR [OPTIONAL CODE] ----------------
 
 for (val in 1:15) {
@@ -49,7 +52,7 @@ for (val in 1:15) {
     labs(x = "Month", y = "Percentage of Active Students", title = str_glue(paste0("Monthly Data for {district_level_ASPM[[",val,"]]$District.Name}"))) +
     geom_hline(yintercept = 100, linetype = "dashed") +
     geom_label(aes(
-      x = max(as.numeric(long_district_level_ASPM$Month)),
+      x = length(long_district_level_ASPM$Month),
       y = 100,
       label = "Max"))
   #geom_label(aes(label = Percent.Active.Students))
@@ -94,7 +97,7 @@ for (val in 1:155) {
     geom_hline(yintercept = specific_school_mean$mean_active_students_school, color = "#FF9900", linetype = "dashed") +
     geom_label(
       aes(
-        x = max(as.numeric(long_school_name_plot$Month)) - 0.5, 
+        x = length(long_school_name_plot$Month) - 0.5, 
         y = specific_school_mean$mean_active_students_school,
         label = glue("Mean ZPP {val}: {round(specific_school_mean$mean_active_students_school, 2)}")
       ),
@@ -150,21 +153,123 @@ for (val in 1:155) {
   school_plot <- ggplot() +
     geom_line(data = percent_data_combined, aes(x = Percent.Month, y = Percent.Active.Students, group = School.Name, color = School.Name)) +
     geom_point(data = percent_data_combined, aes(x = Percent.Month, y = Percent.Active.Students, group = School.Name, color = School.Name)) +
-    geom_label(data = percent_data_combined, aes(x = Percent.Month, y = Percent.Active.Students, label = Percent.Active.Students)) +
+    geom_label(data = percent_data_combined, aes(x = Percent.Month, y = Percent.Active.Students, label = paste0(round(Percent.Active.Students, 1)))) +
     geom_hline(yintercept = 100, color = "blue", linetype = "dashed") +
-    geom_line(data=long_medians, aes(x = Month, y = Median.Percentages, group = 1)) +
-    geom_point(data=long_medians, aes(x = Month, y = Median.Percentages, group = 1)) +
-    geom_label(data = long_medians, aes(x = Month, y = Median.Percentages, label = Median.Percentages))
+    geom_label(aes(
+      x = length(percent_data_combined$Percent.Month) - 0.5,
+      y = 100,
+      label = "Max"
+    )) +
+    scale_y_continuous(breaks = seq(0, 150, by = 10)) +
+    geom_line(data=long_medians, aes(x = Month, y = Median.Percentages, colour = "Median", group = 1)) +
+    geom_point(data=long_medians, aes(x = Month, y = Median.Percentages, colour = "Median", group = 1)) +
+    geom_label(data = long_medians, aes(x = Month, y = Median.Percentages, label = paste0(round(Median.Percentages,1)))) +
+    labs(x = "Month", y = "Active Students as a % of Total Registrations", title = paste0("Percentage Data for ", percent_school_plot$School.Name))
   
   ggsave(
     filename = paste0("School_Percent", val, ".png"),
     plot = school_plot,
     device = "png",
-    path = "PATH/TO/YOUR/FOLDER",
+    path = "D:/Repositories/zpp-analysis-khan-academy/bin",
     width = 1750,
     height = 768,
     units = "px",
     dpi = 100
   )
 }
+
+
+
+## ------------ Hours worked metric ------------
+
+#Need to plot ideal hours - 30 * registered students [Overlay]
+#Calculated - 30 mins * Active Students
+#Real - real life.
+
+
+for (val in 1:155) {
+  string = paste0("ZPP ", val)
+  
+  df_hours <- act_hours[act_hours$School.Name == string,]
+  
+  long_df_hours <- df_hours %>% 
+    pivot_longer(
+      cols = starts_with("ALOKPAS.M"),
+      names_to = ("Month"),
+      values_to = ("Avg.Learning.Time")
+    )
+  #long data format for plotting learning hours.
+  
+  long_df_hours$Month <- factor(long_df_hours$Month, levels = paste0("ALOKPAS.M", 1:12), ordered = TRUE)
+  
+  school_plot <- ggplot() +
+    geom_line(data = long_df_hours, aes(x = Month, y = Avg.Learning.Time, colour = School.Name, group = School.Name)) +
+    geom_point(data = long_df_hours, aes(x = Month, y = Avg.Learning.Time, colour = School.Name, group = School.Name)) +
+    geom_label(data = long_df_hours, aes(x = Month, y = Avg.Learning.Time, label = Avg.Learning.Time)) +
+    geom_hline(yintercept = 30, linetype = "dashed") +
+    geom_label(aes(
+      x = length(long_df_hours$Month) - 2,
+      y = 30,
+      label = "Ideal Learning Time (mins)"
+    )) +
+    labs(x = "Month", y = "Avg. Learning Per Student (mins)", title = paste0("Learning Time data for ", string))
+  
+  # Save the plot
+  ggsave(
+    filename = paste0("School", val, ".png"),
+    plot = school_plot,
+    device = "png",
+    path = "D:/Repositories/zpp-analysis-khan-academy/bin",
+    width = 1750,
+    height = 768,
+    units = "px",
+    dpi = 100
+  )
+}
+
+
+
+
+
+## ------ District Level ---------
+
+for (val in 1:15) {
+  district_number_LH <- val
+  
+  long_district_level_ALOKPAS <- district_level_ALOKPAS[[district_number_LH]] %>%
+    pivot_longer(cols = starts_with("ALOKPAS.M"), 
+                 names_to = "Month", 
+                 values_to = "Avg.Learning")
+  # Change the value inside [[]] to the corresponding district number (1 to 15)
+  
+  long_district_level_ALOKPAS$Month <- factor(long_district_level_ALOKPAS$Month, levels = paste0("ALOKPAS.M", 1:12), ordered = TRUE)
+  
+  plot_dist <- ggplot(data = long_district_level_ALOKPAS, aes(x = Month, y = Avg.Learning, group = School.Name, colour = School.Name)) +
+    geom_line() +
+    geom_point() +
+    labs(x = "Month", y = "Average Learning per Student  (mins) ", title = paste0("Monthly Data for District ", district_number_LH)) +
+    geom_hline(yintercept = 30, linetype = "dashed") +
+    geom_label(aes(
+      x = length(long_df_hours$Month) - 1,
+      y = 30,
+      label = "Ideal Learning Time (mins)"
+    )) +
+    labs(x = "Month", y = "Avg. Learning Per Student (mins)", title = paste0("Learning Time data for District ", district_number_LH))
+  #geom_label(aes(label = Avg.Learning))
+  
+  # Save the plot
+  ggsave(
+    filename = paste0("District", val, ".png"),
+    plot = plot_dist,
+    device = "png",
+    path = "D:/Repositories/zpp-analysis-khan-academy/bin",
+    width = 1750,
+    height = 768,
+    units = "px",
+    dpi = 100
+  )
+}
+
+
+
 rm(temp_df)
